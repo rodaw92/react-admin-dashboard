abe5971e2073153d54dbbe4f15f88d23a8918b29
RAED AWILL <57587026+rodaw92@users.noreply.github.com>
2022-07-04 00:13:42 +0100

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from 'react-dom';
 
-import './index.css' ;
+import './index.css';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root')); // to make sure that I hooked my react application to the root div
