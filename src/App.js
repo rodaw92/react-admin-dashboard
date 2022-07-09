59f94b7edeefdaa5eebc316f40cd762910cec0ed
RAED AWILL <57587026+rodaw92@users.noreply.github.com>
2022-07-09 13:31:00 +0100

    add sidebar basics

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 import './App.css'
 
 const App = () => {
+    const activeMenu = true;
     return (
 
         // this is the power of tailwind by set the div in relative and dark mode by using class name
@@ -16,11 +17,17 @@ const App = () => {
                 <div className='flex relative dark:bg-main-dark-bg'>
                     <div className="fixed right-4 bottom-4" style={{ zIndex: '1000' }}>
                         <TooltipComponent content="Settings" position="Top">
-                            <button type='button' className='text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray'>
+                            <button type='button' className='text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white'
+                                style={{ background: 'blue', borderRadius: '50%' }}>
                                 <FiSettings />
                             </button>
                         </TooltipComponent>
                     </div>
+                    {activeMenu ? (
+                        <div>
+                            Sidebar
+                        </div>
+                    ) : <div> Sidebar w-0 </div>}
 
                 </div>
             </BrowserRouter>
