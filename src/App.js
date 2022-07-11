e97836f0bf025bc552c5359aa8855b7d239ec2fd
RAED AWILL <57587026+rodaw92@users.noreply.github.com>
2022-07-11 13:27:42 +0100

    Add routes to App.js

--- src/App.js
+++ src/App.js
@@ -35,11 +35,42 @@ const App = () => {
                             :
                             'flex-2'}`
 
-
                     }>
+                        <div className='fixed md:static
+                                     bg-main-bg dark:bg-main-dark-bg
+                                     navbar w-full'>NavBar </div>
+                    </div>
+                    <div>
+                        <Routes>
+                            {/* Dashboard */}
+                            <Route path="/" element="ECommerce" />
+                            <Route path="/ecommerce" element="ECommerce" />
 
+                            {/* Pages */}
+                            <Route path="/orders" element="Orders" />
+                            <Route path="/employees" element="Employees" />
+                            <Route path="/customers" element="Customers" />
+
+                            {/* Apps */}
+                            <Route path="/kanban" element="Kanban" />
+                            <Route path="/editor" element="Editor" />
+                            <Route path="/calender" element="Calender" />
+                            <Route path="/color-picker" element="ColorPicker" />
+
+                            {/* charts */}
+                            <Route path="/line" element={<Line />} />
+                            <Route path="/area" element={<Area />} />
+                            <Route path="/bar" element={<Bar />} />
+                            <Route path="/pie" element={<Pie />} />
+                            <Route path="/financial" element={<Financial />} />
+                            <Route path="/color-mapping" element={<ColorMapping />} />
+                            <Route path="/pyramid" element={<Pyramid />} />
+                            <Route path="/stacked" element={<Stacked />} />
+
+                        </Routes>
                     </div>
                 </div>
+
             </BrowserRouter>
         </div>
     )
